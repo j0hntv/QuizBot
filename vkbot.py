fc3222f8acac20485a2cce0449bd115d00d60476
J0hn <j0hntvdesign@gmail.com>
2019-10-07 13:11:38 +0300

    Update vkbot.py

--- vkbot.py
+++ vkbot.py
@@ -4,6 +4,8 @@ import redis
 import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 from random import randrange
 from dotenv import load_dotenv
 from questions import get_questions
@@ -12,11 +14,15 @@ from questions import get_questions
 logger = logging.getLogger('QUIZ-VK')
 
 
-def echo(event, vk_api):
+def start(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        message='Привет! Поиграем?',
+        keyboard=keyboard.get_keyboard(),
+        random_id=get_random_id()
     )
 
 
@@ -46,6 +52,8 @@ if __name__ == "__main__":
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
+
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            start(event, vk_api)
+
